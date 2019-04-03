84e93175e7fe1a20ee908288a57a26e06e6399a9
Gabriela <balicas.gb@gmail.com>
2019-04-03 15:54:21 -0700

    connect app to redux

--- src/App.js
+++ src/App.js
@@ -1,60 +1,32 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import uuid from 'uuid/v4'
+import { connect } from 'react-redux';
+import { addPost, editPost, deletePost } from './actions';
 import NavBar from './NavBar';
 import Routes from './Routes';
 import './App.css';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state={
-      posts: [
-        {
-          title: "Title",
-          description: "Description",
-          body: "body",
-          postId: 0,
-          comments: [
-            {id:uuid(),text:"HAHAHAHA"}, 
-            {id:uuid(),text:"MEow"}
-          ]
-        }
-    ]
-    }
-    this.addPost = this.addPost.bind(this);
-    this.editPost = this.editPost.bind(this);
-    this.deletePost = this.deletePost.bind(this);
-    this.deleteComment = this.deleteComment.bind(this);
-  }
-
-  addPost(postDataObj) {
-    postDataObj.postId = uuid();
-    let currPosts = [...this.state.posts, postDataObj]
-    this.setState({posts: currPosts})
-  }
-
-  editPost(postDataObj) {
-    console.log("IN EDIT", postDataObj)
-    let filteredPosts = this.state.posts.filter(p => p.postId !== postDataObj.postId);
-    let currPosts = [...filteredPosts, postDataObj]
-    this.setState({posts: currPosts})
-  }
-
-  deletePost(id) {
-    let filteredPosts = this.state.posts.filter(p => p.postId !== id);
-    this.setState({posts: filteredPosts})
-  }
-
   render() {
     return (
       <div className="App">
         <NavBar />
-        <Routes triggerAdd={this.addPost} triggerDelete={ this.deletePost } triggerEdit={ this.editPost } posts={this.state.posts} />
+        <Routes triggerAdd={this.props.addPost} triggerDelete={ this.props.deletePost } triggerEdit={ this.props.editPost } posts={this.props.posts} />
       </div>
     );
   }
 }
 
-export default withRouter(App);
+function mapStateToProps(reduxState) {
+  return { posts: reduxState.posts };
+}
+
+const mapDispatchToProps = {
+  addPost,
+  editPost,
+  deletePost
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(App));
