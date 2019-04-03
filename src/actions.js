c418c06235031026e4b019dccd65421bdb1f9958
Gabriela <balicas.gb@gmail.com>
2019-04-03 15:38:47 -0700

    add action gens

---
+++ src/actions.js
@@ -0,0 +1,37 @@
+import { ADD_POST, EDIT_POST, DELETE_POST, ADD_COMMENT, DELETE_COMMENT } from './actionTypes';
+
+
+export function addPost(postDataObj) {
+    return {
+        type: ADD_POST,
+        payload: postDataObj
+    }
+}
+
+export function editPost(postDataObj) {
+    return {
+        type: EDIT_POST,
+        payload: postDataObj
+    }
+}
+
+export function deletePost(postId) {
+    return {
+        type: DELETE_POST,
+        payload: {postId}
+    }
+}
+
+export function addComment(postId, commentText) {
+    return {
+        type: ADD_COMMENT,
+        payload: {postId, commentText},
+    }
+}
+
+export function deleteComment(postId, commentId) {
+    return {
+        type: DELETE_COMMENT,
+        payload: {postId, commentId}
+    }
+}
